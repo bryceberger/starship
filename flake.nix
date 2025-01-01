ddb62b1fd81ff40080644ad2ac85c17aadbd5b66
Bryce Berger <bryce.z.berger@gmail.com>
2024-12-31 21:31:59 -0500

    flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    toolchain = fenix.packages.${system}.complete.toolchain;
+
+    nativeBuildInputs = [
+      toolchain
+      pkgs.cmake
+      pkgs.pkg-config
+    ];
+    buildInputs = [pkgs.openssl.dev];
+  in {
+    devShell.${system} = pkgs.mkShell {
+      name = "starship";
+      inherit nativeBuildInputs buildInputs;
+    };
+  };
+}
